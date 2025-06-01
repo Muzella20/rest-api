bf8118424b06833ba9c954fe20ec5184dc6150e8
Muzella Sabilla Risma <muzellasabillarisma@gmail.com>
2025-06-01 03:43:28 -0700

    Mengubah Tampilan Portfolio

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -188,27 +188,27 @@ foreach ($result['data'] as $photo) {
         <div class="row">
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
+              <img class="card-img-top" src="img/portfolio/2.jpg" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Menganalisis data Covid-19 pada tahun 2020 dengan menggunakan Tools Loker Studio .</p>
               </div>
             </div>
           </div>
 
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
+              <img class="card-img-top" src="img/portfolio/1.jpg" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Kerja Praktek di Bank Nagari Pusat Pengambiran pada Divisi Teknologi & Digitalisasi pada bagian IT Support.</p>
               </div>
             </div>
           </div>
 
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
+              <img class="card-img-top" src="img/portfolio/3.jpg" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Menganalisis data Logbook CSS IT Support Bank Nagari tahun 2020 dengan menggunakan Tools Loker Studio .</p>
               </div>
             </div>
           </div>   
@@ -217,27 +217,27 @@ foreach ($result['data'] as $photo) {
         <div class="row">
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
+              <img class="card-img-top" src="img/portfolio/5.png" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div> 
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                <p class="card-text">Proyek Tugas Kuliah Data mining dengan data online retail menggunakan tools Rapid Miner.
                 </p>
               </div>
             </div>
           </div>
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/portfolio/4.jpeg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Seorang mahasiswa Universitas Islam Negri Imam Bonjol pada Fakultas Saintek dengan jurusan Sistem informasi semester 6.</p>
+              </div>
+            </div>
+          </div> 
 
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
+              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Mencoba mempelajari membuat sebuah program dengan berbagai bahasa pemograman untuk meningkatkan skill.</p>
               </div>
             </div>
           </div>
