0ed00a4c447f986458d409ffeb25590f68660ff4
Muzella Sabilla Risma <muzellasabillarisma@gmail.com>
2025-06-02 04:29:17 -0700

    mengubah tampilan pesan sekarang

--- wpu-hut/latihan1.php
+++ wpu-hut/latihan1.php
@@ -53,7 +53,9 @@ $menu = json_decode($data, true)["menu"];
                 <h5 class="card-title"><?= $row["nama"]; ?></h5>
                 <p class="card-text"><?= $row["deskripsi"]; ?></p>
                 <h5 class="card-title">Rp. <?= $row["harga"]; ?></h5>
-                <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                <div class="d-flex justify-content-center">
+                   <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                </div>
               </div>
             </div>
         </div>
