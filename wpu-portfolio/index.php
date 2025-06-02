a018a448fdfa57b6d03727afe254c275b434e91e
Muzella Sabilla Risma <muzellasabillarisma@gmail.com>
2025-06-02 08:00:34 -0700

    mengedit bagian navbar

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -72,13 +72,19 @@ foreach ($result['data'] as $photo) {
               <a class="nav-link" href="#home">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
+              <a class="nav-link" href="#about">About Me</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#portfolio">Portfolio</a>
+              <a class="nav-link" href="#portfolio">My Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#social">My Social Media</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#myproject">My Project</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
             </li>
 
           </ul>
